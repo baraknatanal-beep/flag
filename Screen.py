2e23227cbaa83069e069cae1d31b15c0509dd0bb
baraknatanal-beep <baraknatanal@gmail.com>
2026-08-13 11:22:08 +0300

    Update Screen.py

--- Screen.py
+++ Screen.py
@@ -18,7 +18,19 @@ def create_soldier(soldier_img):
 
 def draw_soldier(soldier, x, y):
     screen.blit(soldier, (x, y))
+def create_matrix():
+    matrix_surface = pygame.Surface((consts.SCREEN_WIDTH, consts.GRID_ROWS * consts.CELL_SIZE), pygame.SRCALPHA)
+    matrix_surface.fill((0, 0, 0))
+    for r in range(consts.GRID_ROWS + 1):
+        start_pos = (0, r * consts.CELL_SIZE)
+        end_pos = (consts.SCREEN_WIDTH, r * consts.CELL_SIZE)
+        pygame.draw.line(screen, (100, 100, 100), start_pos, end_pos, 1)
 
+    for c in range(consts.GRID_COLS + 1):
+        start_pos = (c * consts.CELL_SIZE, 0)
+        end_pos = (c * consts.CELL_SIZE, consts.GRID_ROWS * consts.CELL_SIZE)
+        pygame.draw.line(screen, (100, 100, 100), start_pos, end_pos, 1)
+    return matrix_surface
 
 def create_grass(grass_img):
     sized_grass = pygame.transform.scale(grass_img, (consts.GRASS_WIDTH, consts.SOLDIER_HEIGHT))
